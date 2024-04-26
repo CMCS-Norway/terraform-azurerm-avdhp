e2ad4ba4b1bd7a12acc020c1bcdd7e829897d8af
Kim Nyhagen <kim.nyhagen@crayon.com>
2024-04-26 10:23:46 +0200

    Update data.tf

--- data.tf
+++ data.tf
@@ -8,14 +8,14 @@ output "azuread_service_principal" {
 
 data "azurerm_subnet" "avd" {
   name = var.avd_subnet_name
-  resource_group_name = "${var.root_name}-connectivity-${var.location}"
-  virtual_network_name = "${var.root_name}${var.avd_vnet}"
+  resource_group_name = var.avd_subnet_rg
+  virtual_network_name = var.avd_vnet
 }
 
 data "azurerm_private_dns_zone" "file" {
   count               = var.create_private_endpoint ? 1 : 0
   name                = var.avd_private_dns_zone
-  resource_group_name = "${var.root_name}-dns"
+  resource_group_name = var.avd_subnet_rg
   provider            = azurerm.connectivity
 }
 
